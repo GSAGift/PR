aaa54e60140b0c44c12e996ebf5d2f935561d973
AmirTalkanov <talkanov@icloud.com>
2025-06-21 20:32:55 +0300

    Реализация QR-кода с задней части машинки

--- src/main.py
+++ src/main.py
@@ -1,21 +1,27 @@
 import mujoco
 from mujoco import viewer
-import numpy as np
+import os
 
-model_pass = "/Users/polinakuranova/uni/robot_programming_course/course_work_2/PR/model/scene.xml"
+# --- Путь к основной сцене ---
+MODEL_FILE = os.path.join(os.path.dirname(__file__), '..', 'model', 'scene.xml')
+
 
 def control_func(model, data):
+    """
+    Основная функция управления. В данный момент пустая,
+    чтобы машина оставалась неподвижной.
+    """
     pass
 
+
 if __name__ == '__main__':
-    # Загрузка модели
-    model = mujoco.MjModel.from_xml_path(model_pass)
+    model = mujoco.MjModel.from_xml_path(MODEL_FILE)
     data = mujoco.MjData(model)
 
     mujoco.set_mjcb_control(control_func)
 
-    # Запуск интерактивного окна
-    with viewer.launch(model, data) as viewer:
+    # Запускаем симуляцию
+    with viewer.launch_passive(model, data) as viewer:
         while viewer.is_running():
             mujoco.mj_step(model, data)
             viewer.sync()
